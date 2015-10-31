e42ec1665cb1346bc21c300eb9857e3db87b06f1
dcamilleri <dorian.camilleri@yahoo.fr>
2015-10-31 01:16:37 -0400

    Add PhantomJS2 support for testing with Travis

--- karma.conf.js
+++ karma.conf.js
@@ -40,8 +40,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'Firefox', 'Safari'],
-    // browsers: ['Chrome'],
+    browsers: ['Chrome', 'Firefox', 'Safari', 'PhantomJS2'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
